4d38d07c75d5dc72a4e332923ce168b9adc45fd0
Brandon Dorner <brandon.dorner@anedot.com>
2023-04-28 22:46:43 -0500

    Add StatuteCondition model

---
+++ backend/app/models/statute_condition.rb
@@ -0,0 +1,2 @@
+class StatuteCondition < ApplicationRecord
+end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -14,6 +14,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_27_163138) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "statute_conditions", force: :cascade do |t|
+    t.string "dependent_url"
+    t.string "type"
+    t.string "text"
+    t.string "statute_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "statutes", force: :cascade do |t|
     t.string "name"
     t.string "url"
