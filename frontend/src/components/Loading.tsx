ceba516614a1d0eaae9b00be4f9a79a836a31e8a
Brandon Dorner <brandon.dorner@anedot.com>
2023-05-10 21:02:16 -0500

    Create Loading component

---
+++ frontend/src/components/Loading.tsx
@@ -0,0 +1,10 @@
+import { Center, Spinner } from '@chakra-ui/react'
+
+const Loading = () => {
+	return (
+		<Center height="100vh" width="100vw">
+			<Spinner />
+		</Center>
+	)
+}
+export default Loading
