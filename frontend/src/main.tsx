33535466cc7fb3e854003d83be72c7bcc8951fc4
Brandon Dorner <brandon.dorner@anedot.com>
2023-05-12 19:44:52 -0500

    Wrap App in QueryClientProvider for `react-query`

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
+
+const queryClient = new QueryClient()
 
 // eslint-disable-next-line import/no-named-as-default-member
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+			<QueryClientProvider client={queryClient}>
+				<App />
+			</QueryClientProvider>
 	</React.StrictMode>
 )
