b25dc1ad6a6599ec222ef42af728180fa61fef00
Brandon Dorner <brandon.dorner@anedot.com>
2023-05-12 19:48:35 -0500

    Wrap app with `BrowserRouter` for react-router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
 const queryClient = new QueryClient()
@@ -9,8 +10,10 @@ const queryClient = new QueryClient()
 // eslint-disable-next-line import/no-named-as-default-member
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
+		<BrowserRouter>
 			<QueryClientProvider client={queryClient}>
 				<App />
 			</QueryClientProvider>
+		</BrowserRouter>
 	</React.StrictMode>
 )
