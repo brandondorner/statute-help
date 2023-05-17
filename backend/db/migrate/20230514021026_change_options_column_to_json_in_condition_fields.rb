7692cc4efeb33e19ceb2a9f5b57c0f6be0181134
Brandon Dorner <brandon.dorner@anedot.com>
2023-05-16 23:59:53 -0500

    Update migration to give condition_fields default

--- backend/db/migrate/20230514021026_change_options_column_to_json_in_condition_fields.rb
+++ backend/db/migrate/20230514021026_change_options_column_to_json_in_condition_fields.rb
@@ -3,11 +3,11 @@
 class ChangeOptionsColumnToJsonInConditionFields < ActiveRecord::Migration[7.0]
   def up
     remove_column :condition_fields, :options
-    add_column :condition_fields, :options, :text
+    add_column :condition_fields, :options, :json, default: []
   end
 
   def down
     remove_column :condition_fields, :options
-    add_column :condition_fields, :options, :string, array: true
+    add_column :condition_fields, :options, :string, default: [], array: true
   end
 end
