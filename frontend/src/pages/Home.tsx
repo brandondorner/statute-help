135b228737b9ba78e6d208a32fbdecac763ae099
Brandon Dorner <brandon.dorner@anedot.com>
2023-05-29 22:37:39 -0500

    Better align Home component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,13 @@
-import { Box } from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react'
 import Header from 'components/Header/Header'
 import Body from 'components/Body/Body'
 
 const Home = () => {
 	return (
-		<Box>
+		<Flex alignItems={'center'} flexDir={'column'} width={'100%'}>
 			<Header />
 			<Body />
-		</Box>
+		</Flex>
 	)
 }
 
