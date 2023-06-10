cf1b77625488f3b5b6809ceaad6d07e05cec7e5b
Brandon Dorner <brandon.dorner@anedot.com>
2023-06-09 19:11:35 -0500

    Add Institutional vandalism RS 14:225 seed
    This seed was added specifically to test the dropdown functionality.

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -99,4 +99,26 @@ Statute.create!(
   url: 'https://www.legis.la.gov/legis/Law.aspx?d=78482'
 )
 
+
+# Institutional vandalism RS 14:225
+rs_14_225 = Statute.create!(
+  conditions: [
+    { damage: 1, sentence: 'The offender shall be fined not more than five hundred dollars, or imprisoned for not more than six months, or both.' },
+    { damage: 2, sentence: 'The offender shall be fined not more than one thousand dollars, or imprisoned, with or without hard labor, for not more than two years or both.' },
+    { damage: 3, sentence: 'The offender shall be fined not more than ten thousand dollars, or imprisoned, with or without hard labor, for not less than one nor more than ten years, or both.' }
+  ],
+  name: 'Institutional Vandalism - RS 14:225',
+  url: 'https://www.legis.la.gov/legis/Law.aspx?d=78375'
+)
+
+rs_14_225.condition_fields.create!(
+  input_name: 'damage',
+  input_type: 'dropdown',
+  text: 'Select damage amount:',
+  options: [
+    { label: 'Less than five hundred follars', value: 1 },
+    { label: 'Between five hundred dollars and fifty thousand dollars', value: 2 },
+    { label: 'More than fifty thousand dollars', value: 3 }
+  ]
+)
 # rubocop:enable Naming/VariableNumber, Layout/LineLength
