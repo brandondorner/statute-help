989c2018c3510653e84a2a631f4d57337a16d572
Brandon Dorner <brandon.dorner@anedot.com>
2023-07-07 18:22:57 -0500

    Add Negligent Arson RS 14.52.2 Statute seed
    This one is important because it deals with multiple dependent condition fields.

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -99,6 +99,55 @@ Statute.create!(
   url: 'https://www.legis.la.gov/legis/Law.aspx?d=78482'
 )
 
+# Negligent Arson - RS 14:52.2
+negligent_arson_rs_14_52_2 = Statute.create!(
+  conditions: [
+    {
+      death: true,
+      sentence: 'Whoever commits the crime of negligent arson resulting in death or serious bodily injury to a human being shall be fined not more than five thousand dollars and imprisoned, with or without hard labor, for not more than five years. In addition, the offender shall be ordered to pay restitution for damages sustained.'
+    },
+    {
+      endangerment: true,
+      sentence: 'Whoever commits the crime of negligent arson where it is foreseeable that human life might be endangered shall be fined not more than three thousand dollars and imprisoned, with or without hard labor, for not more than three years. In addition, the offender shall be ordered to pay restitution for damages sustained.'
+    },
+    {
+      endangerment: false,
+      first_conviction: false,
+      sentence: 'On a second and subsequent conviction, the offender shall be fined not more than two thousand dollars and imprisoned, with or without hard labor, for not more than two years. In addition, the offender shall be ordered to pay restitution for damages sustained.'
+    },
+    {
+      endangerment: false,
+      first_conviction: true,
+      sentence: ' On a first conviction, the offender shall be fined not more than one thousand dollars, or imprisoned for not more than six months, or both. In addition, the offender shall be ordered to pay restitution for damages sustained.'
+    }
+  ],
+  name: 'Negligent Arson - RS 14:52.2',
+  url: 'https://www.legis.la.gov/legis/Law.aspx?d=1106710'
+)
+
+negligent_arson_rs_14_52_2.condition_fields.create!(
+  [
+    {
+      input_name: 'death',
+      input_type: 'checkbox',
+      text: 'Did the crime of negligent arson result in death or serious bodily injury?'
+    },
+    {
+      dependent_condition: 'false',
+      dependent_on: 'death',
+      input_name: 'endangerment',
+      input_type: 'checkbox',
+      text: 'Was the crime of negligent arson commited where it is foreseeable that human life might be endangered?',
+    },
+    {
+      dependent_condition: 'false',
+      dependent_on: 'endangerment',
+      input_name: 'first_conviction',
+      input_type: 'checkbox',
+      text: 'Was this the first conviction of negiglent arson?',
+    }
+  ]
+)
 
 # Institutional vandalism RS 14:225
 rs_14_225 = Statute.create!(
