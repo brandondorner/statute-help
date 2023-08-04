870a2591a197dc4ac984b7db93dfeb53e8c133b6
Brandon Dorner <brandon.dorner@anedot.com>
2023-08-03 23:53:47 -0500

    Move RS 14:38_1 seed to its own file

---
+++ backend/db/seed_data/rs_14_38_1.rb
@@ -0,0 +1,16 @@
+# rubocop:disable Layout/LineLength
+# frozen_string_literal: true
+
+# Mingling Harmful Substances RS 14:38.1
+Statute.create!(
+  sentences: [
+    {
+      sentence: 'Whoever commits the crime of mingling harmful substances shall be imprisoned, with or without hard labor, for not more than two years or fined not more than one thousand dollars, or both.'
+    }
+  ],
+  name: 'RS-14-38-1',
+  title: 'Mingling Harmful Substances - RS 14:38.1',
+  url: 'https://www.legis.la.gov/legis/Law.aspx?d=78492'
+)
+
+# rubocop:enable Layout/LineLength
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,6 +5,7 @@ require_relative 'seed_data/rs_14_37'
 require_relative 'seed_data/rs_14_37_7'
 require_relative 'seed_data/rs_37_4'
 require_relative 'seed_data/rs_14_38'
+require_relative 'seed_data/rs_14_38_1'
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
